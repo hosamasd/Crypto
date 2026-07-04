17daf89b8a04379ca47228232206da0b47cc494e
hosam mohamed <34996617+hosamasd@users.noreply.github.com>
2026-07-04 08:34:35 +0300

    Initial Commit

---
+++ Crypto/CryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoApp.swift
+//  Crypto
+//
+//  Created by hosam on 04/07/2026.
+//
+
+import SwiftUI
+
+@main
+struct CryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
